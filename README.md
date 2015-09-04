654b8f94b2145c538d765a1e85187c408ba13dc0
Tim Cooper <tim.cooper@layeh.com>
2015-09-04 12:43:45 -0300

    change rule format to plain text from JSON

--- README.md
+++ README.md
@@ -16,32 +16,31 @@ Releases are available on the [GitHub project page](https://github.com/bontibon/
 
 ### Rules
 
-A rule file must contain a JSON encoded array of **Rule** objects.  Example:
+A rule file (rules.txt) is a plain text file. Example rule file:
 
-    [
-      {"id": "environment", "description": "Programming Environment"},
-      {"id": "environment-windows", "parent": "environment", "description":
-        "Source code written using Windows", "value": -10},
-      {"id": "execution-missing", "description": "Program execution is missing",
-        "value": -10}
-    ]
+    Programming Environment
+    	Source code written in Windows [-10]
+    	Root user used when creating script file [-2]
+    	Source code [/-15]
+    		Did not include source code [-15]
+    		Style (comments)
+    			Multi-line comments [/-3]
+    				/* not on its own line [-1]
+    				*/ not on its own line [-1]
+    				* not on every line [-1]
+    				No space between * and text on the line [-1]
+    				*'s do no line up vertically [-1]
+    			Redundant comment [-1/-1]
 
-A **Rule** can have the following properties:
+Rule file syntax:
 
-  - *string|integer* `id` (required)
-    - a unique identifier (primary key) for the rule
-  - *string* `description` (required)
-    - a helpful description of the rule
-  - *string|integer* `parent` (optional)
-    - the `id` of the parent rule
-  - *integer* `maximum` (optional)
-    - the maximum value that will be added/deducted for the rule or the rule's children
-  - *integer* `value` (optional)
-    - the point value of the rule (can be positive or negative). Rules that have children cannot has this property.
-  - *integer* `child-count` (optional)
-    - the number of active children required to award the `child-bonus`.
-  - *integer* `child-bonus` (optional)
-    - the bonus value that will be applied to the final mark.
+- One rule per line.
+- Point values for rules are placed at the end of the line inside square brackets.
+- Rules can be nested below other rules by indenting a rule one more than its parent.
+- Rules that have children cannot have point values.
+- Maximum point values for rules AND nested rules can be added in the square brackets with a preceding forward slash (`/`).
+- Indentation must be done using tabs.
+- Lines starting with `#` are comments and are ignored.
 
 ## License
 
--- public/js/collections/Rules.js
+++ public/js/collections/Rules.js
@@ -6,7 +6,84 @@
 define(['underscore', 'backbone', 'models/Rule'], function(_, Backbone, Rule) {
   return Backbone.Collection.extend({
     model: Rule,
-    url: 'api/v1/rules',
+    url: 'rules.txt',
+
+    parse: function(resp) {
+      // TODO: parse: child-bonus, child-count
+      var nextID = 1;
+      var models = [];
+      var parents = [/*
+      {
+        id: _,
+        depth: _,
+      }
+      */];
+      var previous;
+      var lines = resp.split("\n");
+      for (var i = 0; i < lines.length; i++) {
+        var line = lines[i];
+        if (line.trim() == "" || line.startsWith('#')) {
+          continue;
+        }
+        var match = line.match(/^\t*/);
+        var depth = match[0].length;
+        line = line.substring(depth);
+
+        var model = {
+          id: nextID++,
+          depth: depth,
+        };
+
+        if (previous) {
+          var diff = depth - previous.depth;
+          if (diff < 0) {
+            for (var j = diff; j < 0; j++) {
+              parents.pop();
+            }
+            if (parents.length) {
+              model.parent = parents[parents.length - 1].id;
+            }
+          } else if (diff > 0) {
+            parents.push(previous);
+            model.parent = previous.id;
+          } else {
+            if (parents.length) {
+              model.parent = parents[parents.length - 1].id;
+            }
+          }
+        }
+        // TODO: parent
+
+        match = line.match(/\[(-?\d+)?(?:\/(-?\d+))?\]\s*$/);
+        if (match) {
+          model.description = line.substring(0, line.length - match[0].length).trim();
+          if (match[1] !== undefined) {
+            model.value = parseInt(match[1]);
+          }
+          if (match[2] !== undefined) {
+            model.maximum = parseInt(match[2]);
+          }
+        } else {
+          model.description = line.trim();
+        }
+        previous = model;
+        models.push(model);
+      }
+      console.log(models);
+      return models;
+    },
+
+    fetch: function(options) {
+      var collection = this;
+      Backbone.ajax(this.url, {
+        error: function(jqXHR, textStatus, errorThrown) {
+          collection.trigger('error', textStatus);
+        },
+        success: function(data, textStatus, jqXHR) {
+          collection.reset(data, {parse: true});
+        },
+      });
+    },
 
     getCompleteDescription: function(rule) {
       var arr = [];
